6e04ca35be948322a956459fdd6e996d2709cfbd
finnff <finn.fonteijn@student.hu.nl>
2019-11-14 13:58:06 +0100

    yeet

--- week1/1p7.py
+++ week1/1p7.py
@@ -29,8 +29,8 @@ list2 = [1.1, 2.1, 3.82, 4.27, 5.2, 6.2, 7.3, 8.2, 9.9]
 list3 = [1, 2, 3, 4, 5, 'E', 'A', 'z', 9]
 list4 = [1, "test", 3, 4, 5, 'Ez', 'Az', 'UZ', 9]
 
-print(search(list1, 48))
-print(search(list2, 9.9))
-print(search(list3, 'z'))
-print(search(list4, 'Az'))
+print(find(list1, 48))
+print(find(list2, 9.9))
+print(find(list3, 'z'))
+print(find(list4, 'Az'))
 
